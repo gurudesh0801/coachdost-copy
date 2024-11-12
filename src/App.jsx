b0629bf07fa7039dd88fa0b2f17b8a275616a952
gurudesh0801 <111891244+gurudesh0801@users.noreply.github.com>
2024-11-13 00:03:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
@@ -7,28 +12,36 @@ import Footer from "./Components/Footer/Footer";
 import Loading from "./Components/Loading/Loading";
 import Signup from "./Components/Signup/Signup";
 
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {location.pathname !== "/signup" && <Footer />}
+    </>
+  );
+}
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
-  // Handler to hide loading screen and show the main app
   const handleAnimationEnd = () => {
     setIsLoading(false);
   };
+
   return (
-    <>
+    <Router>
       {isLoading ? (
         <Loading onAnimationEnd={handleAnimationEnd} />
       ) : (
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-          <Footer />
-        </Router>
+        <AppContent />
       )}
-    </>
+    </Router>
   );
 }
 
